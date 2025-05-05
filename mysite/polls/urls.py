bf336da28e68de8e8759c0006666c97c95b88e4d
Nokalus <123937808+Nokalus@users.noreply.github.com>
2025-05-05 12:53:13 +0200

    big work
    add table, svin and second page

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('second/', views.second_page, name='second_page'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def index(request):
-    return render(request, '/home/ktimur/site/mysite/polls/templates/home.html')
+    return render(request, 'home.html')
+
+def second_page(request):
+    return render(request, 'second_page.html')
